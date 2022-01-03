2e72f5377459590c4399c64489ef2a0069e8d2c5
rtxys <ftciell@gmail.com>
2022-01-03 22:29:48 +0800

    first commit

---
+++ diy-part2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This is free software
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# ==========luci-app-==========
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# ==========luci-theme-==========
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+# ==========luci-app-==========
+cd ..
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
